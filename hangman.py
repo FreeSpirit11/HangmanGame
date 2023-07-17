2aedb06468a4f015fb891d8feb5e76bd5f5fe872
FreeSpirit11 <139609682+FreeSpirit11@users.noreply.github.com>
2023-07-17 13:49:49 +0530

    Create hangman.py

---
+++ hangman.py
@@ -0,0 +1,97 @@
+import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+end_of_game = False
+word_list = ["ardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+#Testing code
+#print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+lives=6
+while not end_of_game:
+    count=0
+    guess = input("Guess a letter: ").lower()
+
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+        else:
+            count+=1
+            if count==word_length:
+                lives-=1
+                print(stages[lives])
+
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if ("_" not in display) :
+        end_of_game = True
+        print("You win.")
+    if lives==0:
+        end_of_game = True
+        print("you lose")
